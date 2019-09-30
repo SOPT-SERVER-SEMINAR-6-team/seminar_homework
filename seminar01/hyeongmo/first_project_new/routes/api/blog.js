8caa4ef827365326af1e7e6f3b38b461d56ccf73
dgu-pmos <gudah1478@dongguk.edu>
2019-09-30 19:12:03 +0900

    Revert "과제 추가[박형모]"
    This reverts commit ab5fafd94173e0c883d1401037562ea17ea991bf.

--- seminar01/hyeongmo/first_project_new/routes/api/blog.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-router.get('/', (req, res) => {
-    res.send('블로그 입니다.');
-});
-
-module.exports = router;
--- seminar01/hyeongmo/first_project_new/routes/api/cafe.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-router.get('/', (req, res) => {
-    res.send('카페 입니다.');
-});
-
-module.exports = router;
