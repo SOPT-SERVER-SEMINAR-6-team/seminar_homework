4f1dcc4d67d322675aa5aed57b3e9f6f14f0706e
ssionii <ssionii353@gmail.com>
2019-10-03 14:04:37 +0900

    1차 과제 제출 수정2 [양시연]

--- seminar01/siyeon/소스코드/api/news/news.js
+++ seminar01/siyeon/소스코드/api/news/news.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-router.use('/like', require('./like'));
-router.use('/', require('./news'));
+router.get('/', function(req, res) {
+    res.send('news');
+});
 
 module.exports = router;
